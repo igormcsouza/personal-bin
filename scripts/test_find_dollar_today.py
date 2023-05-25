0e42165571d96447cba6897942c0386167e938e1
Igor Souza <igormcsouza@gmail.com>
2023-05-25 12:19:19 -0300

    Update tests for getDollar

--- scripts/test_find_dollar_today.py
+++ scripts/test_find_dollar_today.py
@@ -1,10 +1,14 @@
 """
 Test getDollar main function
 """
+import os
 from unittest.mock import patch
 from datetime import datetime
 
 from freezegun import freeze_time
+
+# Setup a fake environment variable
+os.environ["OPEN_EXCHANGE_APP_ID"] = "something"
 
 import find_dollar_today
 
@@ -18,19 +22,23 @@ def assert_almost_equals(test, real, precision=3):
 
 
 @freeze_time("2023-01-01")
-@patch("forex_python.converter.CurrencyRates.get_rate")
-def test_executes_correctly(get_rate):
-    get_rate.return_value = 5.20
+@patch("find_dollar_today.RetrieveRateOpenExchange.get_today")
+@patch("find_dollar_today.RetrieveRateOpenExchange.get_before")
+def test_executes_correctly(get_before, get_today):
+    get_today.return_value = 5.20
+    get_before.return_value = 5.20
     rate, five_days, first_month_day, one_month = find_dollar_today.main()
 
-    get_rate.assert_called()
+    get_today.assert_called()
+    get_before.assert_called()
 
     assert_equals(rate, 5.20)
     assert_equals(five_days, 0)
     assert_equals(first_month_day, 0)
     assert_equals(one_month, 0)
 
-def get_rate_side_effect(base, simbol, date_obj=None):
+
+def get_before_side_effect(date_obj=None):
     if date_obj:
         if date_obj.day == 5:
             return 5.15
@@ -41,14 +49,21 @@ def get_rate_side_effect(base, simbol, date_obj=None):
 
     return 5.20
 
+def get_today_side_effect():
+    return 5.20
+
+
 @freeze_time("2023-01-10")
-@patch("forex_python.converter.CurrencyRates.get_rate")
-def test_calculates_correctly(get_rate):
-    get_rate.side_effect = get_rate_side_effect
+@patch("find_dollar_today.RetrieveRateOpenExchange.get_today")
+@patch("find_dollar_today.RetrieveRateOpenExchange.get_before")
+def test_calculates_correctly(get_before, get_today):
+    get_before.side_effect = get_before_side_effect
+    get_today.side_effect = get_today_side_effect
 
     rate, five_days, first_month_day, one_month = find_dollar_today.main()
 
-    get_rate.assert_called()
+    get_today.assert_called()
+    get_before.assert_called()
 
     assert_equals(rate, 5.20)
     assert_almost_equals(five_days, 0.97, 2)
